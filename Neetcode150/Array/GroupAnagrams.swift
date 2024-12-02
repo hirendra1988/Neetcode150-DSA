8b893004e0e566233df70a9a6c54467d4d11a819
Hirendra Sharma <hks.it11@gmail.com>
2024-12-02 22:35:50 +0530

    Solve "Group Anagrams" problem using hashmap and sorting

---
+++ Neetcode150/Array/GroupAnagrams.swift
@@ -0,0 +1,31 @@
+//
+//  GroupAnagrams.swift
+//  Neetcode150
+//
+//  Created by Hirendra Sharma on 02/12/24.
+//
+
+//https://leetcode.com/problems/group-anagrams/
+//https://neetcode.io/problems/anagram-groups
+class GroupAnagrams {
+    
+    init() {
+        runTest()
+    }
+    
+    //Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+    func runTest() {
+        let strs = ["eat","tea","tan","ate","nat","bat"]
+        let result = groupAnagrams(strs)
+        print(result)
+    }
+    
+    // Time Complexity: O(n * mlogm) || Space Compelxity: O(n*m)
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var result = [String: [String]]()
+        for str in strs {
+            result["\(str.sorted())", default: []] += [str]
+        }
+        return Array(result.values)
+    }
+}
