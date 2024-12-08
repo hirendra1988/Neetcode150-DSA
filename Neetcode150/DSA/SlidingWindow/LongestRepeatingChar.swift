483257869f14875ac1702cb30e8828e003b48710
Hirendra Sharma <hks.it11@gmail.com>
2024-12-08 16:53:16 +0530

    Solve "Longest Repeating Character Replacement" problem on Leetcode: 424

---
+++ Neetcode150/DSA/SlidingWindow/LongestRepeatingChar.swift
@@ -0,0 +1,43 @@
+//
+//  LongestRepeatingChar.swift
+//  Neetcode150
+//
+//  Created by Hirendra Sharma on 08/12/24.
+//
+
+//https://leetcode.com/problems/longest-repeating-character-replacement/
+//https://neetcode.io/problems/longest-repeating-substring-with-replacement
+class LongestRepeatingChar {
+    
+    init() {
+        runTest()
+    }
+    
+    func runTest() {
+        let s = "ABAB", k = 2
+        let result = characterReplacement(s, k)
+        print(result)
+    }
+    
+    // Time Complexity: O(n) || Space Compelxity: O(n)
+    func characterReplacement(_ s: String, _ k: Int) -> Int {
+        var hashMap = [Character: Int]()
+        let charArray = Array(s)
+        var left = 0
+        var maxFreq = 0
+        var res = 0
+        for i in 0..<charArray.count  {
+            hashMap[charArray[i], default: 0] += 1
+            maxFreq = max(maxFreq, hashMap[charArray[i], default: 0])
+            let window = i - left + 1
+            if (window - maxFreq) <= k {
+                res = max(res, window)
+            } else {
+                hashMap[charArray[left], default: 0] -= 1
+                left += 1
+            }
+        }
+        return res
+    }
+    
+}
