1266b763fa059f7dfb2545a68fe6f778834a0a80
Hirendra Sharma <hks.it11@gmail.com>
2024-12-23 20:44:17 +0530

    Solve "Binary Search" problem on Leetcode: 704

---
+++ Neetcode150/DSA/Binary Search/BinarySearch.swift
@@ -0,0 +1,47 @@
+//
+//  BinarySearch.swift
+//  Neetcode150
+//
+//  Created by Hirendra Sharma on 23/12/24.
+//
+
+//https://leetcode.com/problems/binary-search/
+//https://neetcode.io/problems/binary-search
+class BinarySearch {
+    
+    init() {
+        runTest()
+    }
+    
+    func runTest() {
+        let nums = [-1,0,3,5,9,12], target = 9
+        let res = search(nums, target)
+        print(res)
+    }
+    
+    // Time Complexity: O(logn) || Space Compelxity: O(1)
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        while left <= right {
+            let mid = (left + right)/2
+            if nums[mid] == target {
+                return mid
+            }
+            if nums[left] == target {
+                return left
+            }
+            if nums[right] == target {
+                return right
+            }
+            
+            if nums[mid] > target {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return -1
+    }
+    
+}
