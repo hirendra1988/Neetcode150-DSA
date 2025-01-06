593b73333ca32ffe88cebba282c1172130930175
Hirendra Sharma <hks.it11@gmail.com>
2025-01-06 18:46:21 +0530

    Solve "Maximum Depth of Binary Tree" problem on Leetcode: 104

--- Neetcode150/DSA/Tree/MaximumDepthofBinaryTree.swift
+++ Neetcode150/DSA/Tree/MaximumDepthofBinaryTree.swift
@@ -1,13 +1,13 @@
 //
-//  InvertBinaryTree.swift
+//  MaximumDepthofBinaryTree.swift
 //  Neetcode150
 //
 //  Created by Hirendra Sharma on 06/01/25.
 //
 
-//https://leetcode.com/problems/invert-binary-tree/
-//https://neetcode.io/problems/invert-a-binary-tree
-class InvertBinaryTree {
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+//https://neetcode.io/problems/depth-of-binary-tree
+class MaximumDepthofBinaryTree {
     
     class TreeNode {
         var left: TreeNode?
@@ -19,54 +19,39 @@ class InvertBinaryTree {
     }
     
     init() {
-//        Input: root = [4,2,7,1,3,6,9]
-//        Output: [4,7,2,9,6,3,1]
+//        Input: root = [3,9,20,null,null,15,7]
+//        Output: 3
         runTest()
     }
     
     func runTest() {
         let root = createRootTree()
         displayLevelOrder(root: root)
-        let res = invertTree(root)
-        displayLevelOrder(root: res)
+        let res = maxDepth(root)
+        print(res)
     }
     
     //Time Complexity: O(n) || Space Compelxity: O(n) // O(logn) for a balanced binary tree
-    func invertTree(_ root: TreeNode?) -> TreeNode? {
-        guard let root = root else { return nil }
-        (root.left, root.right) = (root.right, root.left)
-        _ = invertTree(root.left)
-        _ = invertTree(root.right)
-        return root
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        let left = maxDepth(root.left)
+        let right = maxDepth(root.right)
+        return max(left, right) + 1
     }
     
     func createRootTree() -> TreeNode? {
-        let n1 = TreeNode(4)
-        let n2 = TreeNode(2)
-        let n3 = TreeNode(7)
+        let n1 = TreeNode(3)
+        let n2 = TreeNode(9)
+        let n3 = TreeNode(20)
         n1.left = n2
         n1.right = n3
         
-        let n4 = TreeNode(1)
-        let n5 = TreeNode(3)
+        let n4 = TreeNode(15)
+        let n5 = TreeNode(17)
         n2.left = n4
         n2.right = n5
         
-        let n6 = TreeNode(6)
-        let n7 = TreeNode(9)
-        n3.left = n6
-        n3.right = n7
-        
         return n1
-    }
-    
-    // using preorder
-    func displayPreorder(root: TreeNode?) {
-        if root != nil {
-            print(root!.val, terminator: " -> ")
-            displayPreorder(root: root?.left)
-            displayPreorder(root: root?.right)
-        }
     }
     
     func displayLevelOrder(root: TreeNode?) {
