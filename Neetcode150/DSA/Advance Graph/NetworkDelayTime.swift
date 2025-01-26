34023ab2132a82b1e678cc62fda5feca4c7e33f5
Hirendra Sharma <hks.it11@gmail.com>
2025-01-26 23:51:48 +0530

    Code refactore

--- Neetcode150/DSA/Advance Graph/NetworkDelayTime.swift
+++ Neetcode150/DSA/Advance Graph/NetworkDelayTime.swift
@@ -68,49 +68,5 @@ class NetworkDelayTime {
     func addDEdge(_ u: Int, _ v: Int, _ weight: Int, _ adj: inout [[Node]]) {
         adj[u].append(Node(v, weight))
     }
-    
-//    func networkDelayTime(_ times: [[Int]], _ n: Int, _ k: Int) -> Int {
-//
-//        var adj = [[Node]](repeating: [Node](), count: n + 1)
-//        var block = [Bool](repeating: false, count: n + 1)
-//        var d = [Int](repeating: Int.max, count: n + 1)
-//        
-//        d[k] = 0 // Start from node k
-//
-//        for time in times {
-//            addDEdge(time[0], time[1], time[2], &adj)
-//        }
-//        
-//        for _ in 0..<n {
-//            var u = -1
-//            for j in 1...n { // Nodes are 1-indexed
-//                if !block[j] && (u == -1 || d[j] < d[u]) {
-//                    u = j
-//                }
-//            }
-//            
-//            if u == -1 { // No reachable node left
-//                break
-//            }
-//            
-//            block[u] = true
-//            
-//            for node in adj[u] {
-//                if !block[node.vertex] && d[u] != Int.max && (d[node.vertex] > (d[u] + node.weight)) {
-//                    d[node.vertex] = d[u] + node.weight
-//                }
-//            }
-//        }
-//        let maxDelay = d[1...n].max()!
-//        return maxDelay == Int.max ? -1 : maxDelay
-//    }
-//    
-//    func addDEdge(_ u: Int, _ v: Int, _ weight: Int, _ adj: inout [[Node]]) {
-//        adj[u].append(Node(v, weight))
-//    }
-    
-    func display() {
-        
-    }
-    
+
 }
