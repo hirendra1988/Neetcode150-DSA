c35930f8248a83b00d26b2dfcba9372cf83df773
Hirendra Sharma <hks.it11@gmail.com>
2025-01-31 16:16:10 +0530

    Solve "Task Scheduler" problem on Leetcode: 621

---
+++ Neetcode150/DSA/Priority Queue/TaskScheduler.swift
@@ -0,0 +1,39 @@
+//
+//  TaskScheduler.swift
+//  Neetcode150
+//
+//  Created by Hirendra Sharma on 31/01/25.
+//
+
+//https://leetcode.com/problems/task-scheduler/
+//https://neetcode.io/problems/task-scheduling
+//https://www.youtube.com/watch?v=p1usK_Cb8uE
+class TaskScheduler {
+    
+    init() {
+//        Input: tasks = ["A","A","A","B","B","B"], n = 2
+//        Output: 8
+        runTest()
+    }
+    
+    func runTest() {
+        let tasks: [Character] = ["A","A","A","B","B","B"], n = 2
+        let res = leastInterval(tasks, n)
+        print(res)
+    }
+    
+    // Time Complexity: O(n) || Space Compelxity: O(1)
+    func leastInterval(_ tasks: [Character], _ n: Int) -> Int {
+        var freqArr = [Int](repeating: 0, count: 26)
+        for task in tasks {
+            let index = Int(task.asciiValue! - Character("A").asciiValue!)
+            freqArr[index] += 1
+        }
+        let maxFreq = freqArr.max()!
+        let maxFreqCount = freqArr.filter { $0 == maxFreq }.count
+        let idleTime = (maxFreq - 1) * (n + 1) + maxFreqCount
+        return max(idleTime, tasks.count)
+        
+    }
+    
+}
