97a20b9227ad7a74d43119345f65cef514069982
Hirendra Sharma <hks.it11@gmail.com>
2025-02-23 21:11:02 +0530

    Solve "Meeting Rooms" problem on Leetcode: 252

---
+++ Neetcode150/DSA/Intervals/MeetingRooms.swift
@@ -0,0 +1,47 @@
+//
+//  MeetingRooms.swift
+//  Neetcode150
+//
+//  Created by Hirendra Sharma on 23/02/25.
+//
+
+//https://leetcode.com/problems/meeting-rooms/
+//https://neetcode.io/problems/meeting-schedule
+class MeetingRooms {
+    
+    init() {
+//        Example 1:
+//        Input: intervals = [[0,30],[5,10],[15,20]]
+//        Output: false
+        
+//        Example 2:
+//        Input: intervals = [[7,10],[2,4]]
+//        Output: true
+        runTest()
+    }
+    
+    func runTest() {
+        let intervals = [[0,30],[5,10],[15,20]]
+        let result = canAttendMeetings(intervals)
+        print(result)
+    }
+    
+    //Time Complexity: O(nlogn) || Space Compelxity: O(n)
+    func canAttendMeetings(_ intervals: [[Int]]) -> Bool {
+        if intervals.isEmpty {
+            return true
+        }
+        let sortedInterval = intervals.sorted { $0[0] < $1[0] }
+        var lastPoint = Int.min
+        for i in 0..<sortedInterval.count {
+            let interval = sortedInterval[i]
+            if lastPoint <= interval[0] {
+                lastPoint = interval[1]
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+}
