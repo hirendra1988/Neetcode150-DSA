545e36d27181477013763e9baf343f8a8f0f8cb8
Hirendra Sharma <hks.it11@gmail.com>
2025-02-26 16:38:50 +0530

    Solve "Set Matrix Zeroes" problem with space optimised on Leetcode: 73

--- Neetcode150/DSA/MathAndGeomatry/SetMatrixZeroes.swift
+++ Neetcode150/DSA/MathAndGeomatry/SetMatrixZeroes.swift
@@ -7,6 +7,7 @@
 
 //https://leetcode.com/problems/set-matrix-zeroes/
 //https://neetcode.io/problems/set-zeroes-in-matrix
+//https://www.youtube.com/watch?v=dSxt3ZCbIqA
 class SetMatrixZeroes {
     
     init() {
@@ -21,8 +22,50 @@ class SetMatrixZeroes {
         print(matrix)
     }
     
-    // MARK: Time Complexity: O(m*n) || Space Compelxity: O(m+n)
+    // MARK: Time Complexity: O(m*n) || Space Compelxity: O(1)
     func setZeroes(_ matrix: inout [[Int]]) {
+        let rows = matrix.count
+        let cols = matrix[0].count
+        var firstRow = false
+        var firstCol = false
+        for row in 0..<rows {
+            for col in 0..<cols {
+                if matrix[row][col] == 0 {
+                    if row == 0 {
+                        firstRow = true
+                    }
+                    if col == 0 {
+                        firstCol = true
+                    }
+                    matrix[0][col] = 0
+                    matrix[row][0] = 0
+                }
+            }
+        }
+        
+        for row in 1..<rows {
+            for col in 1..<cols {
+                if matrix[0][col] == 0 || matrix[row][0] == 0 {
+                    matrix[row][col] = 0
+                }
+            }
+        }
+        
+        if firstRow {
+            for col in 0..<cols {
+                matrix[0][col] = 0
+            }
+        }
+        
+        if firstCol {
+            for row in 0..<rows {
+                matrix[row][0] = 0
+            }
+        }
+    }
+    
+    // MARK: Time Complexity: O(m*n) || Space Compelxity: O(m+n)
+    func setZeroes1(_ matrix: inout [[Int]]) {
         let rows = matrix.count
         let cols = matrix[0].count
         var rowMap = [Bool](repeating: false, count: rows)
