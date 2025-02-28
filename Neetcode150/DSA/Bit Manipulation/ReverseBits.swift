509427f5cb8c316ea4115dd576230617f2bba15c
Hirendra Sharma <hks.it11@gmail.com>
2025-02-28 16:22:32 +0530

    Solve "Reverse Bits" problem on Leetcode: 190

---
+++ Neetcode150/DSA/Bit Manipulation/ReverseBits.swift
@@ -0,0 +1,39 @@
+//
+//  ReverseBits.swift
+//  Neetcode150
+//
+//  Created by Hirendra Sharma on 28/02/25.
+//
+
+//https://leetcode.com/problems/reverse-bits/
+//https://neetcode.io/problems/reverse-bits
+//https://www.youtube.com/watch?v=LJiMk8lPJNc
+class ReverseBits {
+    
+    init() {
+//        Input: n = 00000010100101000001111010011100
+//        Output:    964176192 (00111001011110000010100101000000)
+//        Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+        runTest()
+    }
+    
+    func runTest() {
+        let n: Int32 = 0b00000010100101000001111010011100
+        let res = reverseBits(n)
+        print(res)
+    }
+    
+    //Time Complexity: O(1) || Space Compelxity: O(1)
+    func reverseBits(_ n: Int32) -> Int32 {
+        var res: Int32 = 0
+        var num = n
+        for i in 1...32 {
+            let lsb = num & 1 //Get the least significant bit
+            let reverseLsb = lsb << (32-i) // // Reverse the bit position
+            res = res | reverseLsb // Set the reversed bit
+            num = num >> 1 // Right shift the number
+        }
+        return res
+    }
+    
+}
--- Neetcode150/DSA/Bit Manipulation/Video.swift
+++ Neetcode150/DSA/Bit Manipulation/Video.swift
@@ -5,3 +5,32 @@
 //  Created by Hirendra Sharma on 27/02/25.
 //
 //https://www.youtube.com/watch?v=Yk3Jwm5YuFs&t=22s
+//https://chatgpt.com/c/67c14493-eab8-8006-9a5d-451930d6b43a
+//https://www.youtube.com/watch?v=XbjQ-heGd58
+//🔥 Shortcut Method (Fast Trick)
+//Just memorize these:
+//
+//Decimal    Binary
+//2^0 = 1    0001
+//2^1 = 2    0010
+//2^2 = 4    0100
+//2^3 = 8    1000
+//2^4 = 16    10000
+//2^5 = 32    100000
+
+//Steps:
+//Find the largest power of 2 less than 10
+//✅ 8 → 2³
+//
+//Subtract 8 from 10
+//10 - 8 = 2
+//
+//Next largest power of 2 is 2 → 2¹
+//
+//Subtract 2 - 2 = 0
+//Now we're done!
+//
+//Now Write the Binary Number:
+//2³(8)    2²(4)    2¹(2)    2⁰(1)
+// 1         0        1        0
+//
