df0e4fe67b0ba5a8f83ebc8e6542f7e4031a1511
Hirendra Sharma <hks.it11@gmail.com>
2025-03-09 15:40:16 +0530

    Test Happy number DSA Problem

--- Neetcode150/ViewController.swift
+++ Neetcode150/ViewController.swift
@@ -12,8 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        AlienDictionary()
+        //AlienDictionary()
+        print(isHappy(19))
        
+    }
+    
+    func isHappy(_ n: Int) -> Bool {
+        if n == 1 || n == 7 || n == 10 {
+            return true
+        }
+        var tempNum = 0
+        var num = n
+        while num > 0 {
+            let remainder = num % 10
+            tempNum += remainder * remainder
+            num = num/10
+        }
+        return isHappy(tempNum)
     }
 
 }
