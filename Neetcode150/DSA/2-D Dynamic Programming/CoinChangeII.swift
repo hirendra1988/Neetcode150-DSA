eec6d59fc5f7e3b0fd0bcf9a2482a461804dcaf2
Hirendra Sharma <hks.it11@gmail.com>
2025-04-13 19:49:59 +0530

    Updated code

--- Neetcode150/DSA/2-D Dynamic Programming/CoinChangeII.swift
+++ Neetcode150/DSA/2-D Dynamic Programming/CoinChangeII.swift
@@ -10,13 +10,13 @@
 class CoinChangeII {
     
     init() {
-//        Input: amount = 5, coins = [1,2,5]
-//        Output: 4
-//        Explanation: there are four ways to make up the amount:
-//        5=5
-//        5=2+2+1
-//        5=2+1+1+1
-//        5=1+1+1+1+1
+        //        Input: amount = 5, coins = [1,2,5]
+        //        Output: 4
+        //        Explanation: there are four ways to make up the amount:
+        //        5=5
+        //        5=2+2+1
+        //        5=2+1+1+1
+        //        5=1+1+1+1+1
         runTest()
     }
     
