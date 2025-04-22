b05b530e0e3a4e32711f0783ee6edb575fb60347
Hirendra Sharma <hks.it11@gmail.com>
2025-04-22 22:18:35 +0530

    Updated code

--- Neetcode150/DSA/1-D Dynamic Programming/HouseRobber.swift
+++ Neetcode150/DSA/1-D Dynamic Programming/HouseRobber.swift
@@ -11,10 +11,10 @@
 class HouseRobber {
     
     init() {
-//        Input: nums = [1,2,3,1]
-//        Output: 4
-//        Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-//        Total amount you can rob = 1 + 3 = 4.
+        //        Input: nums = [1,2,3,1]
+        //        Output: 4
+        //        Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+        //        Total amount you can rob = 1 + 3 = 4.
         runTest()
     }
     
