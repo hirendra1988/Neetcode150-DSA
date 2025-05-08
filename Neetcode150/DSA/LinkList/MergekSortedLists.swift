8bef5e986de1d12e78bb185e1f262275b3252ba4
Hirendra Sharma <hks.it11@gmail.com>
2025-05-08 22:03:34 +0530

    Updated code

--- Neetcode150/DSA/LinkList/MergekSortedLists.swift
+++ Neetcode150/DSA/LinkList/MergekSortedLists.swift
@@ -19,16 +19,16 @@ class MergekSortedLists {
     }
     
     init() {
-//        Input: lists = [[1,4,5],[1,3,4],[2,6]]
-//        Output: [1,1,2,3,4,4,5,6]
-//        Explanation: The linked-lists are:
-//        [
-//          1->4->5,
-//          1->3->4,
-//          2->6
-//        ]
-//        merging them into one sorted list:
-//        1->1->2->3->4->4->5->6
+        //        Input: lists = [[1,4,5],[1,3,4],[2,6]]
+        //        Output: [1,1,2,3,4,4,5,6]
+        //        Explanation: The linked-lists are:
+        //        [
+        //          1->4->5,
+        //          1->3->4,
+        //          2->6
+        //        ]
+        //        merging them into one sorted list:
+        //        1->1->2->3->4->4->5->6
         runTest()
     }
     
@@ -79,7 +79,7 @@ class MergekSortedLists {
         }
         return lists[0]
     }
-
+    
     func mergeTwoSortedList(_ node1: ListNode?, _ node2: ListNode?) -> ListNode? {
         let dummyNode = ListNode(0)
         var currentNode: ListNode? = dummyNode
