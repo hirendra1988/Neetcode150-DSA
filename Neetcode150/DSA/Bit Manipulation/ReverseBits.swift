e2ce91f6be02ac2dafe1c431372e8a6b91685624
Hirendra Sharma <hks.it11@gmail.com>
2025-05-11 10:27:13 +0530

    Code updated

--- Neetcode150/DSA/Bit Manipulation/ReverseBits.swift
+++ Neetcode150/DSA/Bit Manipulation/ReverseBits.swift
@@ -11,9 +11,9 @@
 class ReverseBits {
     
     init() {
-//        Input: n = 00000010100101000001111010011100
-//        Output:    964176192 (00111001011110000010100101000000)
-//        Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+        //        Input: n = 00000010100101000001111010011100
+        //        Output:    964176192 (00111001011110000010100101000000)
+        //        Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
         runTest()
     }
     
