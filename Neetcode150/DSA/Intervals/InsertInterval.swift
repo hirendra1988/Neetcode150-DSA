6ddb07807eeff6aa31e3aa672a575242a40a5caf
Hirendra Sharma <hks.it11@gmail.com>
2025-05-21 21:37:57 +0530

    InsertInterval solution updated

--- Neetcode150/DSA/Intervals/InsertInterval.swift
+++ Neetcode150/DSA/Intervals/InsertInterval.swift
@@ -45,5 +45,34 @@ class InsertInterval {
         }
         return result
     }
+
+    func insert2(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        let n = intervals.count
+        var i = 0
+        var newInterval = newInterval
+        
+        // Add all intervals before newInterval
+        while i < n, intervals[i][1] < newInterval[0] {
+            result.append(intervals[i])
+            i += 1
+        }
+        
+        // Merge overlapping intervals
+        while i < n , intervals[i][0] <= newInterval[1] {
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        }
+        result.append(newInterval)
+        
+        // Add remaining intervals
+        while i < n {
+            result.append(intervals[i])
+            i += 1
+        }
+        
+        return result
+    }
     
 }
