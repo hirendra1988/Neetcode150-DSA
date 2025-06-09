fb28cf36017386e84e406e85dad8248d8260cfcd
Hirendra Sharma <hks.it11@gmail.com>
2025-06-09 22:38:12 +0530

    Code updated

--- Neetcode150/DSA/Bit Manipulation/MissingNumber.swift
+++ Neetcode150/DSA/Bit Manipulation/MissingNumber.swift
@@ -10,10 +10,10 @@
 class MissingNumber {
     
     init() {
-//        Input: nums = [3,0,1]
-//        Output: 2
-//        Explanation:
-//        n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+        //        Input: nums = [3,0,1]
+        //        Output: 2
+        //        Explanation:
+        //        n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
         runTest()
     }
     
