a00b33afc89a3b7a9bcbffed5c8481357430d3d8
Hirendra Sharma <hks.it11@gmail.com>
2025-07-16 19:21:09 +0530

    32. Longest Valid Parentheses - Problem solved

---
+++ Neetcode150/DSA/Bonus/Stack/LongestValidParentheses.swift
@@ -0,0 +1,43 @@
+//
+//  Untitled.swift
+//  Neetcode150
+//
+//  Created by Hirendra Sharma on 16/07/25.
+//
+
+//https://leetcode.com/problems/longest-valid-parentheses/description/
+class LongestValidParentheses {
+    
+    init() {
+        runTest()
+    }
+    
+    func runTest() {
+        let s = ")()())"
+        let res = longestValidParentheses(s)
+        print(res)
+        //Output: 4
+    }
+    
+    func longestValidParentheses(_ s: String) -> Int {
+        var sChars = Array(s)
+        var stack = [Int]()
+        stack.append(-1)
+        var result = 0
+        for i in 0..<sChars.count {
+            if sChars[i] == "(" {
+                stack.append(i)
+            } else {
+                stack.popLast()
+                if stack.isEmpty {
+                    stack.append(i)
+                } else {
+                    result = max(result, i - stack.last!)
+                }
+            }
+        }
+        return result
+    }
+    
+    
+}
